12cc6be208ea92f0bc2197a1388fd0b1488bbd71
Mohammad Azeem <--replace-all>
2024-09-15 08:32:05 +0530

    >> hit counter  << added⛈

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import TimeStamp from '@/components/TimeStamp';
-
+import { readFile, writeFile } from '@/helpers/file-helpers';
 export const dynamic = 'force-dynamic';
 
-function Home(){
-  // const [time, setTime] = React.useState(null);
-  const time = new Date().toLocaleString()
+const DATABASE_PATH = "/src/database.json";
 
-  // React.useEffect(() => {
-  //   setTime(new Date().toLocaleTimeString())
-  // }, []); // runs just hydration time (first time render)
-  
+function Home(){
+  const time = new Date().toLocaleString()
+  let { hits } = JSON.parse(readFile(DATABASE_PATH));
+  hits++;
+  writeFile(DATABASE_PATH, JSON.stringify({ hits }));
+
   return (
     <> 
     <main className="mt-5">
       <div className="px-3 py-4 bg-yellow-100 border border-orange-500 rounded-md">
           <h1>Greetings Fellas</h1>
       </div>
+      <p className='my-8 font-mono'>
+        You are visitor number:
+        {' '}
+        <span className='p-1 bg-yellow-100 rounded-sm'>{hits}</span>  
+      </p>
     </main>
 
     <TimeStamp className="mb-14" time={time}/>
